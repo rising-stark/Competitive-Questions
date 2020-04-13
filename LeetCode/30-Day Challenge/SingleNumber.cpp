f4d6b12eb79d07cadee87b86d9b97790d5743740
Harshit Kumar <harshitkk02@gmail.com>
2020-04-13 22:51:53 +0530

    Create SingleNumber.cpp

---
+++ LeetCode/30-Day Challenge/SingleNumber.cpp
@@ -0,0 +1,31 @@
+https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/528/week-1/3283/
+
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+
+```
+class Solution {
+public:
+    int singleNumber(vector<int>& arr) {
+        int i,n, x=0;
+        n=arr.size();
+        for(i=0;i<n;i++){
+            x^=arr[i];
+        }
+        return x;
+    }
+};
+
+```
