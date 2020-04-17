8cd39eda567fac226f403f7674d0bd4c61043fe4
Harshit Kumar <harshitkk02@gmail.com>
2020-04-17 23:07:00 +0530

    Update and rename NumberOfIslands.cpp to Day17: NumberOfIslands.cpp

--- LeetCode/30-Day Challenge/Day17: NumberOfIslands.cpp
+++ LeetCode/30-Day Challenge/Day17: NumberOfIslands.cpp
@@ -1,4 +1,4 @@
-
+https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3302/
 
 Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
