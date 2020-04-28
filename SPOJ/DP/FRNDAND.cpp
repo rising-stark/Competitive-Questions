a8d569387cb149cc505c7561594683b5b3ac72fe
Harshit Kumar <harshitkk02@gmail.com>
2020-04-28 08:12:37 +0530

    Create FRNDAND.cpp

---
+++ SPOJ/DP/FRNDAND.cpp
@@ -0,0 +1,21 @@
+https://www.spoj.com/problems/FRNDZND/
+
+#include <stdio.h>
+
+int main() {
+	int n,q,i;
+	scanf("%d %d", &n, &q);
+	int arr[n];
+	for(i=0;i<n;i++){
+		scanf("%d", &arr[i]);
+	}
+	for(i=0;i<q;i++){
+		int l,r,ans=0;
+		scanf("%d %d", &l, &r);
+		if(r==l){
+			ans=arr[l-1];
+		}
+		printf("%d\n",ans);
+	}
+	return 0;
+}
