fbdeec5ae3d7bf33dbd04203e2d6995fe352eb58
Harshit Kumar <harshitkk02@gmail.com>
2020-07-05 06:39:44 +0530

    Create Micro_and_Prime_Prime.cpp
    Data Structures, sieve

---
+++ Hackerearth/Micro_and_Prime_Prime.cpp
@@ -0,0 +1,259 @@
+/*
+The function to look up for the logic/solution is void solve() at the bottom.
+Above the solve function is main function which calls the solve function simply.
+https://www.hackerearth.com/practice/math/number-theory/primality-tests/practice-problems/algorithm/micro-and-prime-prime-1/description/
+
+Problem Description:
+
+Micro just learned about prime numbers. But he quickly got bored of them, so he defined a new kind of numbers and called them Prime Prime Numbers.
+A number X is Prime Prime if number of prime numbers from 1 to X (inclusive) are prime.
+Now he wants to find out the number of Prime Prime numbers from L to R (inclusive). Help Micro with it.
+
+Input:
+First line consists of a single integer T denoting number of test cases
+Following T lines consists of two space separated integers denoting L and R
+
+Output:
+Print the number of Prime Prime Numbers for each between L and R for each test case in a new line.
+
+Constraints:
+1 <= T <= 10^5
+1 <= L, R <= 10^6
+
+Sample Input:
+2
+3 10
+4 12
+
+SAMPLE OUTPUT 
+4
+5
+
+
+Time Limit:	1.0 sec(s) for each input file.
+Memory Limit:	256 MB
+Source Limit:	1024 KB
+*/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define ALL(x) x.begin(), x.end()
+#define maxx(a,b,c) max(a, max(b,c))
+#define minn(a,b,c) min(a, min(b,c))
+using namespace std;
+const int mod=1e9+7;
+const int inf = (1<<30);
+string toBin(ll a);
+void printMat(int arr[], int n);
+void printMat(vector<int> arr, int n);
+void printMat(vector<vector<int> > arr, int n, int m);
+void printMat(vector<int> arr[], int n);
+void solve();
+//void printMat(int arr[][], int n, int m);
+void printMap(unordered_map<int, int> mp);
+void printMap(unordered_map<ll, ll> mp);
+void printMap(unordered_map<char, int> mp);
+void printMap(unordered_map<string, int> mp);
+void printMap(map<int, int> mp);
+void printMap(map<ll, ll> mp);
+void printMap(map<char, int> mp);
+void printMap(map<string, int> mp);
+void printSet(set<int> s);
+void printSet(set<ll> s);
+void printSet(set<char> s);
+void printSet(set<string> s);
+void printSet(unordered_set<int> s);
+void printSet(unordered_set<ll> s);
+void printSet(unordered_set<char> s);
+void printSet(unordered_set<string> s);
+string toBin(ll a){
+	string s="";
+	while(a>0){
+		if(a&1){
+			s="1"+s;
+		}else{
+			s="0"+s;
+		}
+		a>>=1;
+	}
+	return s;
+}
+void printMat(int arr[], int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void printMat(vector<int> arr, int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void printMat(vector<int> arr[], int row, int col){
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<arr[i][j]<<" ";
+		}cout<<endl;
+	}cout<<endl;
+}
+void printMat(vector<vector<int> > arr, int row, int col){
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<arr[i][j]<<" ";
+		}cout<<endl;
+	}cout<<endl;
+}
+void printMap(unordered_map<int, int> mp){
+	unordered_map<int, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(unordered_map<ll, ll> mp){
+	unordered_map<ll, ll>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(unordered_map<char, int> mp){
+	unordered_map<char, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(unordered_map<string, int> mp){
+	unordered_map<string, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<int, int> mp){
+	map<int, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<ll, ll> mp){
+	map<ll, ll>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<char, int> mp){
+	map<char, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<string, int> mp){
+	map<string, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printSet(set<int> s){
+	set<int>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(set<ll> s){
+	set<ll>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(set<char> s){
+	set<char>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it);
+	}cout<<endl;
+}
+void printSet(set<string> s){
+	set<string>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(unordered_set<int> s){
+	unordered_set<int>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(unordered_set<ll> s){
+	unordered_set<ll>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(unordered_set<char> s){
+	unordered_set<char>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it);
+	}cout<<endl;
+}
+void printSet(unordered_set<string> s){
+	unordered_set<string>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	#endif
+	int t;
+	//cin>>t;
+	t=1;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
+
+void solve(){
+	int i, j, n;
+	n=1000000;
+	bitset<1000001> arr(0);
+	std::vector<int> prime;
+	unordered_set<int> s;
+	arr[0]=arr[1]=1;
+	for(i=2;i*i<=n;i++){
+		if(arr[i]==1)
+			continue;
+		for(j=i*i;j<=n;j+=i){
+			arr[j]=1;
+		}
+	}
+	int c=0;
+	for(i=1;i<=n;i++){
+		if(arr[i]==0){
+			c++;
+			s.insert(i);
+		}
+		if(s.find(c)!=s.end()){
+			prime.pb(i);
+		}
+	}
+	//printMat(prime, prime.size());
+	int t;
+	cin>>t;
+	while(t--){
+		int l, r;
+		cin>>l>>r;
+		cout<<upper_bound(prime.begin(), prime.end(), r)-upper_bound(prime.begin(), prime.end(), l-1)<<endl;
+	}
+}
