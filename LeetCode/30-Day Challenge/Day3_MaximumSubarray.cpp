2b4208a94afc4586ae4f2a8648bc8b63856b69d6
Harshit Kumar <harshitkk02@gmail.com>
2020-08-01 22:05:42 +0530

    Update and rename Day3: MaximumSubarray.cpp to Day3_MaximumSubarray.cpp

--- LeetCode/30-Day Challenge/Day3_MaximumSubarray.cpp
+++ LeetCode/30-Day Challenge/Day3_MaximumSubarray.cpp
@@ -1,3 +1,4 @@
+/*
 https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/528/week-1/3285/
 
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
@@ -11,7 +12,7 @@ Follow up:
 
 If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 
-```
+*/
 class Solution {
 public:
     int maxSubArray(vector<int>& arr) {
@@ -26,4 +27,3 @@ public:
         return ma;
     }
 };
-```
