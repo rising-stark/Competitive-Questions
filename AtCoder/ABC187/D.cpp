c267817fbf69cbd42a6fd9a73bcd08c2c86ffff2
rising-stark <harshitkk02@gmail.com>
2021-01-02 19:10:55 +0530

    AtCoder Beginner Contest 187. Solved 3/6 A B C

---
+++ AtCoder/ABC187/D.cpp
@@ -0,0 +1,257 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vector<int> >
+#define vvll vector<vector<ll> >
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pis pair<int, string>
+#define psi pair<string, int>
+#define ALL(x) x.begin(), x.end()
+#define maxx(a,b,c) max(a, max(b,c))
+#define minn(a,b,c) min(a, min(b,c))
+using namespace std;
+const int mod=1e9+7;
+const int inf = (1<<30);
+void solve();
+string toBin(ll a);
+void printMat(int arr[], int n);
+void printMat(vector<int> arr, int n);
+void printMat(vector<vector<int> > arr, int n, int m);
+void printMat(vector<vector<ll> > arr, int n, int m);
+void printMat(vector<int> arr[], int n);
+void printMat(vector<ll> arr[], int n);
+void printMat(int arr[][2], int n, int m);
+void printMat(ll arr[][2], int n, int m);
+void printMap(unordered_map<int, int> mp);
+void printMap(unordered_map<ll, ll> mp);
+void printMap(unordered_map<char, int> mp);
+void printMap(unordered_map<string, int> mp);
+void printMap(map<int, int> mp);
+void printMap(map<ll, ll> mp);
+void printMap(map<char, int> mp);
+void printMap(map<string, int> mp);
+void printSet(set<int> s);
+void printSet(set<ll> s);
+void printSet(set<char> s);
+void printSet(set<string> s);
+void printSet(unordered_set<int> s);
+void printSet(unordered_set<ll> s);
+void printSet(unordered_set<char> s);
+void printSet(unordered_set<string> s);
+string toBin(ll a){
+	string s="";
+	while(a>0){
+		if(a&1){
+			s="1"+s;
+		}else{
+			s="0"+s;
+		}
+		a>>=1;
+	}
+	return s;
+}
+void printMat(int arr[], int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void printMat(vector<int> arr, int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void printMat(vector<int> arr[], int row, int col){
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<arr[i][j]<<" ";
+		}cout<<endl;
+	}cout<<endl;
+}
+void printMat(vector<vector<int> > arr, int row, int col){
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<arr[i][j]<<" ";
+		}cout<<endl;
+	}cout<<endl;
+}
+void printMat(vector<vector<ll> > arr, int row, int col){
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<arr[i][j]<<" ";
+		}cout<<endl;
+	}cout<<endl;
+}
+void printMat(ll arr[][2], int row, int col){
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<arr[i][j]<<" ";
+		}cout<<endl;
+	}cout<<endl;
+}
+void printMap(unordered_map<int, int> mp){
+	unordered_map<int, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(unordered_map<ll, ll> mp){
+	unordered_map<ll, ll>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(unordered_map<char, int> mp){
+	unordered_map<char, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(unordered_map<string, int> mp){
+	unordered_map<string, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<int, int> mp){
+	map<int, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<ll, ll> mp){
+	map<ll, ll>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<char, int> mp){
+	map<char, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<string, int> mp){
+	map<string, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printSet(set<int> s){
+	set<int>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(set<ll> s){
+	set<ll>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(set<char> s){
+	set<char>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it);
+	}cout<<endl;
+}
+void printSet(set<string> s){
+	set<string>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(unordered_set<int> s){
+	unordered_set<int>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(unordered_set<ll> s){
+	unordered_set<ll>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(unordered_set<char> s){
+	unordered_set<char>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it);
+	}cout<<endl;
+}
+void printSet(unordered_set<string> s){
+	unordered_set<string>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("C:/Users/ujjwa/Desktop/Practice/code/Competitive-Questions/input.txt", "r", stdin);
+	freopen("C:/Users/ujjwa/Desktop/Practice/code/Competitive-Questions/output.txt", "w", stdout);
+	#endif
+	int t=1;
+	//scanf("%d", &t);
+	//cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
+bool comp(pair<ll, ll> &a, pair<ll, ll> &b){
+	if(a.first>b.first || (a.first==b.first && (a.second>b.second)))
+		return true;
+	return false;
+}
+bool comp1(pair<ll, ll> &a, pair<ll, ll> &b){
+	if(a.second>b.second || (a.second==b.second && (a.first>b.first)))
+		return true;
+	return false;
+}
+void solve(){
+	int i, j, n;
+	cin>>n;
+	vector<pair<ll, ll> > arr(n);
+	ll suma=0, sumab=0;
+	for(i=0;i<n;i++){
+		ll a, b;
+		cin>>a>>b;
+		arr[i].first=a+b;
+		arr[i].second=a;
+		suma+=a;
+		sumab+=a;
+	}
+	sort(arr.begin(), arr.end(), comp);
+	ll c=0;
+	i=0;
+	int ans=n;
+	while(c<=suma){
+		c+=arr[i].first;
+		suma-=arr[i].second;
+		i++;
+	}
+	ans=min(ans, i);
+
+	sort(arr.begin(), arr.end(), comp1);
+	c=0;
+	i=0;
+	while(c<=sumab){
+		c+=arr[i].first;
+		sumab-=arr[i].second;
+		i++;
+	}
+	ans=min(ans, i);
+	cout<<ans<<endl;	
+}
