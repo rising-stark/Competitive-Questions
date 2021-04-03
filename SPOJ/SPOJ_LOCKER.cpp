80dbf07ce89c21c07cb0cb6afbcd64a604fe5421
rising-stark <harshitkk02@gmail.com>
2021-04-04 00:49:00 +0530

    Practice solved SPOJ_LOCKER.cpp

---
+++ SPOJ/SPOJ_LOCKER.cpp
@@ -0,0 +1,243 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define ALL(x) x.begin(), x.end()
+#define maxx(a,b,c) max(a, max(b,c))
+#define minn(a,b,c) min(a, min(b,c))
+using namespace std;
+const int mod=1e9+7;
+const int inf = (1<<30);
+string toBin(ll a);
+void printMat(int arr[], int n);
+void printMat(vector<int> arr, int n);
+void printMat(vector<vector<int> > arr, int n, int m);
+void printMat(vector<int> arr[], int n);
+ll solve(ll n);
+//void printMat(int arr[][], int n, int m);
+void printMap(unordered_map<int, int> mp);
+void printMap(unordered_map<ll, ll> mp);
+void printMap(unordered_map<char, int> mp);
+void printMap(unordered_map<string, int> mp);
+void printMap(map<int, int> mp);
+void printMap(map<ll, ll> mp);
+void printMap(map<char, int> mp);
+void printMap(map<string, int> mp);
+void printSet(set<int> s);
+void printSet(set<ll> s);
+void printSet(set<char> s);
+void printSet(set<string> s);
+void printSet(unordered_set<int> s);
+void printSet(unordered_set<ll> s);
+void printSet(unordered_set<char> s);
+void printSet(unordered_set<string> s);
+string toBin(ll a){
+	string s="";
+	while(a>0){
+		if(a&1){
+			s="1"+s;
+		}else{
+			s="0"+s;
+		}
+		a>>=1;
+	}
+	return s;
+}
+void printMat(int arr[], int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void printMat(vector<int> arr, int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void printMat(vector<int> arr[], int row, int col){
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<arr[i][j]<<" ";
+		}cout<<endl;
+	}cout<<endl;
+}
+void printMat(vector<vector<int> > arr, int row, int col){
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<arr[i][j]<<" ";
+		}cout<<endl;
+	}cout<<endl;
+}
+void printMap(unordered_map<int, int> mp){
+	unordered_map<int, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(unordered_map<ll, ll> mp){
+	unordered_map<ll, ll>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(unordered_map<char, int> mp){
+	unordered_map<char, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(unordered_map<string, int> mp){
+	unordered_map<string, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<int, int> mp){
+	map<int, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<ll, ll> mp){
+	map<ll, ll>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<char, int> mp){
+	map<char, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printMap(map<string, int> mp){
+	map<string, int>::iterator it;
+	for(it=mp.begin();it!=mp.end();it++){
+		cout<<it->first<<" -->"<<it->second<<endl;
+	}
+	cout<<endl;
+}
+void printSet(set<int> s){
+	set<int>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(set<ll> s){
+	set<ll>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(set<char> s){
+	set<char>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it);
+	}cout<<endl;
+}
+void printSet(set<string> s){
+	set<string>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(unordered_set<int> s){
+	unordered_set<int>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(unordered_set<ll> s){
+	unordered_set<ll>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+void printSet(unordered_set<char> s){
+	unordered_set<char>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it);
+	}cout<<endl;
+}
+void printSet(unordered_set<string> s){
+	unordered_set<string>::iterator it;
+	for(it=s.begin();it!=s.end();it++){
+		cout<<(*it)<<" ";
+	}cout<<endl;
+}
+ll dp[10000000];
+ll M=mod;
+ll binPow(ll a, ll n) {
+	if(n == 0)
+		return 1;
+	if(n == 1)
+		return a%M;
+	ll r = binPow(a, n/2);
+	if(n % 2 == 0) {
+		return (r%M * r%M)%M;
+	}
+	else 
+		return (r%M * r%M * a%M)%M;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	#endif
+	int t;
+	cin>>t;
+	//t=1;
+	for(ll i=1;i<10000000;i++){
+		dp[i]=max(i, dp[i/2]*dp[i- i/2])%mod;
+	}
+	while(t--){
+		ll n;
+		cin>>n;
+		cout<<solve(n)%mod<<endl;
+	}
+	return 0;
+}
+/*ll solve(ll n){
+	if(n<=4){
+		return n;
+	}
+	if(n<10000000){
+		if(dp[n]!=0){
+			return dp[n];
+		}
+	}
+
+	if(n&1){
+		return (solve(n/2)*1LL*solve(n-n/2))%mod;
+	}else{
+		ll b=solve(n/2);
+		return (b*1LL*b)%mod;
+	}
+}*/
+
+ll solve(ll n){
+	ll ans, n1;
+	if(n < 3) {
+		return n;
+	}
+	if(n%3 == 1) {
+		n1 = (n - 4) / 3;
+		ans = 4*binPow(3, n1);
+	}
+	else if(n%3 == 2){
+		n1 = (n - 2) / 3;
+		ans = 2*binPow(3, n1);
+	}
+	else 
+		ans = binPow(3, n/3);
+	ans %= M;
+	return ans;
+}
