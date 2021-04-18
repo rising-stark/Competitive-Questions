a1116ce72eaff4f47af64d943df7e9e1c9b9d3bf
rising-stark <harshitkk02@gmail.com>
2021-04-18 17:47:36 +0530

    Practice solved UVa 534 - Frogger.cpp

---
+++ UVa/UVa 534 - Frogger.cpp
@@ -0,0 +1,132 @@
+//#include "bits/stdc++.h"
+#include <vector>
+#include <utility>
+#include <set>
+#include <map>
+#include <queue>
+#include <string>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <bitset>
+#include <iomanip>
+#define ll long long int
+#define pb push_back
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vll vector<ll>
+#define vvi vector<vector<int> >
+#define vvc vector<vector<char> >
+#define vvs vector<vector<string> >
+#define vvll vector<vector<ll> >
+#define vvpii vector<vector<pii> >
+#define vvpll vector<vector<pll> >
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define ff first
+#define ss second
+#define vpii vector<pair<int, int> >
+#define vpll vector<pair<ll, ll> >
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define maxx(a, b, c) max((a), max((b), (c)))
+#define minn(a, b, c) min((a), min((b), (c)))
+#define dup(x) (x).resize(distance((x).begin(), unique(all(x))))
+#define maxn 100001
+#define null nullptr
+#define mem memset(dp, -1, sizeof dp)
+#define mem1 memset(dp1, -1, sizeof dp1)
+#define fori for(int i=0;i<n;i++)
+#define forj for(int j=0;j<m;j++)
+#define w(x) cout<<(#x)<<"="<<(x)<<" "
+const ll mod=((ll)1e9)+7;
+const ll inf = (1<<30);
+const double eps = (double)1e-6;
+const double pi = 3.14159265358979323;
+
+using namespace std;
+
+template <typename T1, typename T2, typename T3>struct trio{T1 x; T2 y; T3 z; trio(){};trio(T1 _x, T2 _y, T3 _z):x(_x), y(_y), z(_z){}};
+#define tri trio<int,int,int>
+#define trll trio<ll,ll,ll>
+
+//STL pair input
+template<typename T1, typename T2>inline istream& operator >> (istream& os, pair<T1, T2>& t){ os >> t.first; os >> t.second; return os; }
+template<typename T>inline istream &operator>>(istream &os, vector<T>& v){for(T &i:v){cin>>i;}return os;}
+template<typename T>inline istream &operator>>(istream &os, vector<vector<T>>& v){for(vector<T> &i : v){cin>>i;}return os;}
+
+//STL debug by print
+template<typename T1, typename T2>inline ostream& operator << (ostream& os, const pair<T1, T2>& t){ return os << t.first << " " << t.second<<endl; }
+template<typename T1, typename T2, typename T3>inline ostream& operator << (ostream& os, const trio<T1, T2, T3>& t){ return os << t.x << " " << t.y << " " << t.z<<endl; }
+template<typename T>void printarr(T arr[], int n){for(int i=0;i<n;i++)cout<<arr[i]<<" ";cout<<endl;}
+template<typename T>inline ostream &operator<<(ostream &os,const vector<T>& v){for(T i : v){os<<i<<" ";}return os<<endl;}
+template<typename T>inline ostream &operator<<(ostream &os,const vector<vector<T>>& v){for(vector<T> i : v){os<<i;}return os;}
+template<typename T>inline ostream &operator<<(ostream &os,const set<T>&v){for(T i : v){os<<i<<" ";}return os<<endl;}
+template<typename T>inline ostream &operator<<(ostream &os,const set<T, greater<T> >&v){for(T i : v){os<<i<<" ";}return os<<endl;}
+template<typename T>inline ostream &operator<<(ostream &os,const multiset<T>&v){for(T i : v){os<<i<<" ";}return os<<endl;}
+template<typename T>inline ostream &operator<<(ostream &os,const multiset<T, greater<T> >&v){for(T i : v){os<<i<<" ";}return os<<endl;}
+template<typename T1, typename T2>inline ostream &operator << (ostream & os,const map<T1, T2>& v){for(pair<T1, T2> i : v){os<<i;}return os<<endl;}
+template<typename T1, typename T2>inline ostream &operator << (ostream & os,const multimap<T1, T2>& v){for(pair<T1, T2> i : v){os<<i;}return os<<endl;}
+void printgraph(vi arr[], int n){for(int i=0;i<n;i++){cout<<i<<" --> ";cout<<arr[i];}cout<<endl;}
+inline ll binexp(ll a, ll b, ll m){a%=m;ll res=1;while(b){if(b&1)res=(res*a)%m;a=(a*a)%m;b>>=1;}return res;}
+inline string toBase(ll a,ll b=2){string s="";if(!a)return"0";while(a){s=to_string(a%b)+s;a/=b;}return s;}
+inline ll digsum(ll n){ll ans=0;while(n){ans+=n%10;n/=10;}return ans;}
+inline ll sq(ll a){return a*a;}
+inline bool ispow2(ll i){return i && (i & -i) == i;}
+inline bool isdigit(char c){if(c>='0' && c<='9')return true;return false;}
+inline bool isupper(char c){if(c>='A' && c<='Z')return true;return false;}
+inline bool islower(char c){if(c>='a' && c<='z')return true;return false;}
+void solve(int );
+
+ll binsearch(ll l, ll r, vll arr, ll x){
+	ll ans=0;
+	while(l<=r){
+		ll m = l+((r-l)>>1);
+		if(m){
+			//ans=max(ans, m);
+			l=m+1;
+		}else{
+			//ans=min(ans, m);
+			r=m-1;
+		}
+	}
+	return ans;
+}
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("C:/Users/ujjwa/Desktop/Practice/code/Competitive-Questions/input.txt", "r", stdin);
+	freopen("C:/Users/ujjwa/Desktop/Practice/code/Competitive-Questions/output.txt", "w", stdout);
+	#endif
+	int i=0, t=1;
+	//int stress = 1;
+	//cin>>t;
+	//cout<<t<<endl;
+	//generateSieve();
+	int n;
+	cin>>n;
+	while(n){
+		cout<<"Scenario #"<<(++i)<<endl;
+		solve(n);
+		cin>>n;
+	}
+	//if(stress==1){compare();}
+	return 0;
+}
+double dist(pii a, pii b){
+	return sqrt(sq(a.ff-b.ff) + sq(a.ss-b.ss));
+}
+void solve(int n){
+	ll i, j, k;
+	int m=n;
+	vector<vector<double>> dp(n, vector<double>(n, 0.0));
+	vpii arr(n);
+	cin>>arr;
+	fori forj dp[i][j]=dist(arr[i], arr[j]);
+	for(k=0;k<n;k++) fori forj dp[i][j]=min(dp[i][j], max(dp[i][k],dp[k][j]));
+	cout<<"Frog Distance = "<<fixed<<setprecision(3)<<dp[0][1]<<endl<<endl;
+}
